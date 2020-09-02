afc282ec61f6cbb284fcc2e34ce001b21dff8e61
gokul-sarath07 <gokul510.s@gmail.com>
2020-09-02 19:36:43 +0530

    Last name added

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 echo "User Registration Problem"
-read -p "Enter a email id: " id
-shopt -s extglob
+
+function firstname {
+read -p "Enter first name: " first
 regex="^[A-Z][a-z]{2,}$"
-if [[ $id =~ $regex ]]
+if [[ $first =~ $regex ]]
 then
 	echo Yes
 else
 	echo No
 fi
+}
+
+function lastname {
+read -p "Enter last name: " last
+regex="^[A-Z][a-z]{2,}$"
+if [[ $last =~ $regex ]]
+then
+	echo Yes
+else
+	echo No
+fi
+}
+
+function main {
+	firstname
+	lastname
+}
+main
