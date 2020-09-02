a767f4e3954905f646c1e44478fb0574a27db92b
gokul-sarath07 <gokul510.s@gmail.com>
2020-09-02 19:24:45 +0530

    First Name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,2 +1,11 @@
-#!/bin/bash -x
+#!/bin/bash
 echo "User Registration Problem"
+read -p "Enter a email id: " id
+shopt -s extglob
+regex="^[A-Z][a-z]{2,}$"
+if [[ $id =~ $regex ]]
+then
+	echo Yes
+else
+	echo No
+fi
